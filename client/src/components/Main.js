8b9706a03c44dd3a47fd2d4e9f89d6adb0db2a92
chrhfoq123 <chrhfoq123@naver.com>
2023-01-15 19:22:43 +0900

    메인 ui 제작 준비

---
+++ client/src/components/Main.js
@@ -0,0 +1,12 @@
+function Main()
+{
+    return(
+        <div className="main-content">
+            <div className="main-header">
+                지역 선택
+            </div>
+        </div>
+    )
+}
+
+export default Main;

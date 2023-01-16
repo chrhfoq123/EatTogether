be88893f14204c4ee59e6ef20e9df33a7a0eb886
chrhfoq123 <chrhfoq123@naver.com>
2023-01-16 19:14:59 +0900

    메인화면 메뉴 선택 ui 제작

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -3,7 +3,32 @@ function Main()
     return(
         <div className="main-content">
             <div className="main-header">
-                지역 선택
+                <div className="select-location">
+                    지역 선택 ▼
+                </div>
+                <div className="myinfo">
+                    <img src={'./public/img/person.png'}></img>
+                </div>
+            </div>
+            <div className="option-content">
+                <div className="select-menu1">
+                    일식그림
+                </div>
+                <div className="select-menu2">
+                    중식그림
+                </div>
+                <div className="select-menu3">
+                    양식그림
+                </div>
+                <div className="select-menu4">
+                    한식그림
+                </div>
+                <div className="select-menu5">
+                    고기그림
+                </div>
+                <div className="select-menu6">
+                    치킨그림
+                </div>
             </div>
         </div>
     )

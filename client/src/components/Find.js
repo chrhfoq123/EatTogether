af212f9e77f480e133af82e51e643ff5175bf4d0
chrhfoq123 <chrhfoq123@naver.com>
2023-01-19 02:09:35 +0900

    매칭중 ui 제작

---
+++ client/src/components/Find.js
@@ -0,0 +1,18 @@
+function Find()
+{
+    return(
+        <div className="find-content">
+            <div className="find-message">
+                같은 상대를 찾는중 ...
+            </div>
+            <div className="pass-minute">
+                ~분 경과
+            </div>
+            <div className="cancel-btn">
+                매칭 취소
+            </div>
+        </div>
+    )
+}
+
+export default Find;
